d1337124107d83e9c4eb1c12eda401d93c2e1590
Elyse Kriegel <esamkriegel@gmail.com>
2020-01-30 00:40:21 -0600

    add azure

--- server/index.js
+++ server/index.js
@@ -1,25 +1,31 @@
-const express = require("express")
-const next = require("next"); 
+const express = require("express");
+const next = require("next");
 
 const dev = process.env.NODE_ENV !== "production";
-const nextApp = next({ dev }); 
+const nextApp = next({ dev });
 const handle = nextApp.getRequestHandler();
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-nextApp.prepare().then( () => {
+nextApp
+  .prepare()
+  .then(() => {
     const app = express();
 
-    app.get('/my-custom-route', (req, res) => {
-        res.status(200).json({hello: "Hello, from the back-end world!"})
-    })
+    app.get("/my-custom-route", (req, res) => {
+      res.status(200).json({ hello: "Hello, from the back-end world!" });
+    });
 
-    app.get('*', (req, res) => {
-        return handle(req,res); 
-    })
+    app.get("*", (req, res) => {
+      return handle(req, res);
+    });
 
     app.listen(port, err => {
-        if (err) throw err;
-        console.log(`> Ready on localhost: ${port}`); 
-    })
-})
+      if (err) throw err;
+      console.log(`> Ready on localhost: ${port}`);
+    });
+  })
+  .catch(ex => {
+    console.err(ex.stack);
+    process.exit(1);
+  });
