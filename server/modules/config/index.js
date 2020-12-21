9e09cc362a47a6bd60231ef64c2d9ab43c1a0133
Elyse Kriegel <55773029+elyskrie21@users.noreply.github.com>
2020-12-21 00:17:24 -0600

    Update index.js

--- server/modules/config/index.js
+++ server/modules/config/index.js
@@ -1,8 +1,8 @@
-const user = 'esamkriegel@gmail.com'
-const refreshToken = '1//04dfQmt2M67CXCgYIARAAGAQSNwF-L9Ir1YOZm_GVgFs2aR9cfsg36RRBTVl-jT6BH0aPmilyaq8-UZ15Ln105z7A03LLn23xxtE'
-const accessToken = '4/xAHaRmFqAOHPi0UzIWYxj-5optcZFSgFNK4Fp0gEiNR4JF2KCb5EYsjOiUnNsgdURgW2uXnclVkO6poVMC3qttI'
-const clientId = '580503745452-s7tfgpl8fgi30g8td75fl08es14conhn.apps.googleusercontent.com'
-const clientSecret = '9m3194qT-DQfRKHsDizmtKCc'
+const user = 'MY_USER'
+const refreshToken = process.env.REFRESH_TOKEN
+const accessToken = process.env.ACCESS_TOKEN
+const clientId = process.env.CLIENT_ID
+const clientSecret = process.env.CLIENT_SECRET
 
 //These are for test purposes only
 export default {
